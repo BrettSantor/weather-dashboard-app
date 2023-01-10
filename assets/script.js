1c63d3cd3a41dce5910935668f8539ffb898120f
BrettSantor <b.santor27@gmail.com>
2023-01-09 17:23:03 -0800

    fetch api

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,39 @@
-var art =document.createElement('article')
+var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=Morongo Valley&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c'
 
-var head3 =document.createElement('h3')
+var forcast = 'https://api.openweathermap.org/data/2.5/forecast?q=palm springs&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c'
 
-head3.innerHTML = "Hey We're Linked!"
+var headline = document.querySelector('#topHeader')
+var bod = document.querySelector('#landingSpot')
 
-art.append(head3)
+function getCurrApi(currentWeather){
+    fetch(currentWeather)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            headline.textContent = data.name;
+        });
+        
+}
 
-document.body.append(art)
+getCurrApi(currentWeather);
+
+function getForApi(forcast){
+    fetch(forcast)
+        .then(function (rsponse) {
+            console.log(rsponse);
+            return rsponse.json();
+        })
+        .then(function(datr){
+            console.log(datr.);
+            
+               
+                
+
+            
+        });
+}
+getForApi(forcast);
+
