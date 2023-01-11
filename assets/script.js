0edcf1997ebe147628fb05250cadc4c0797a2878
BrettSantor <b.santor27@gmail.com>
2023-01-11 09:38:07 -0800

    add cities to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
-var subBut = document.querySelector('#inputForm')
-var tempSlot = document.querySelector('#currTemp')
-var headline = document.querySelector('#topHeader')
-var bod = document.querySelector('#bulk')
+var subBut = document.querySelector('#inputForm');
+var tempSlot = document.querySelector('#currTemp');
+var headline = document.querySelector('#topHeader');
+var bod = document.querySelector('#bulk');
+var currCity = {};
+var storeCity = []
 
 function getCurrApi(location){
-    var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c'
+    var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c';
     fetch(currentWeather)
         .then(function (response) {
             console.log(response);
@@ -20,8 +22,8 @@ function getCurrApi(location){
 }
 
 function getForApi(location){
-    var datrArr = []
-var forecast = 'https:api.openweathermap.org/data/2.5/forecast?q=' + location + '&cnt=40&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c'
+    var datrArr = [];
+var forecast = 'https:api.openweathermap.org/data/2.5/forecast?q=' + location + '&cnt=40&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c';
     fetch(forecast)
         .then(function (rsponse) {
             console.log(rsponse);
@@ -29,15 +31,15 @@ var forecast = 'https:api.openweathermap.org/data/2.5/forecast?q=' + location + 
         })
         .then(function(datr){
             console.log(datr.list);
-            console.log(datr.list.length)
+            console.log(datr.list.length);
          for(var i = 0; i < datr.list.length; i++) {
-           datrArr.push(datr.list[i].main)
+           datrArr.push(datr.list[i].main);
             // console.log(datrArr)
             //   console.log(datr.list[i].main)
             
         } console.log(datrArr)
             for(var j = 3; j < datrArr.length; j += 8){
-             console.log(datrArr[j])
+             console.log(datrArr[j]);
              var forCards = document.createElement('div');
              forCards.classList.add('card');
             forCards.textContent = 'temp: ' + datrArr[j].temp;
@@ -48,11 +50,12 @@ var forecast = 'https:api.openweathermap.org/data/2.5/forecast?q=' + location + 
 
   subBut.addEventListener('submit', function(event){
     event.preventDefault();
-    oldCity = localStorage.getItem('city')
-         var location = event.target[0].value
-            console.log(location)
+         var location = event.target[0].value;
+            console.log(location);
     if (location){
-        localStorage.setItem('city', location);
+        currCity = {city: location}
+        storeCity.push(currCity)
+        localStorage.setItem('cityObj', JSON.stringify(storeCity));
      getCurrApi(location);
       getForApi(location);
 }
