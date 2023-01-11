967627d45fd3b92ffcc19f63c805bf347dd13b49
BrettSantor <b.santor27@gmail.com>
2023-01-10 22:53:38 -0800

    add city current temp and forecast to page

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,59 @@
-var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=Morongo Valley&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c'
-
-var forcast = 'https://api.openweathermap.org/data/2.5/forecast?q=palm springs&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c'
-
+var subBut = document.querySelector('#inputForm')
+var tempSlot = document.querySelector('#currTemp')
 var headline = document.querySelector('#topHeader')
-var bod = document.querySelector('#landingSpot')
+var bod = document.querySelector('#bulk')
 
-function getCurrApi(currentWeather){
+function getCurrApi(location){
+    var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c'
     fetch(currentWeather)
         .then(function (response) {
             console.log(response);
             return response.json();
         })
         .then(function(data){
-            console.log(data);
-            headline.textContent = data.name;
+            console.log(data.main.temp);
+            headline.textContent = data.name + " " + data.main.temp;
+        
+           
         });
         
 }
 
-getCurrApi(currentWeather);
-
-function getForApi(forcast){
-    fetch(forcast)
+function getForApi(location){
+    var datrArr = []
+var forecast = 'https:api.openweathermap.org/data/2.5/forecast?q=' + location + '&cnt=40&units=imperial&appid=a85e2d69a404ab1f087da912eb4b924c'
+    fetch(forecast)
         .then(function (rsponse) {
             console.log(rsponse);
             return rsponse.json();
         })
         .then(function(datr){
-            console.log(datr.);
+            console.log(datr.list);
+            console.log(datr.list.length)
+         for(var i = 0; i < datr.list.length; i++) {
+           datrArr.push(datr.list[i].main)
+            // console.log(datrArr)
+            //   console.log(datr.list[i].main)
             
-               
-                
+        } console.log(datrArr)
+            for(var j = 3; j < datrArr.length; j += 8){
+             console.log(datrArr[j])
+             var forCards = document.createElement('div');
+             forCards.classList.add('card');
+            forCards.textContent = 'temp: ' + datrArr[j].temp;
+             bod.appendChild(forCards);
+           }
+          });
+  }
 
-            
-        });
+  subBut.addEventListener('submit', function(event){
+    event.preventDefault();
+    oldCity = localStorage.getItem('city')
+         var location = event.target[0].value
+            console.log(location)
+    if (location){
+        localStorage.setItem('city', location);
+     getCurrApi(location);
+      getForApi(location);
 }
-getForApi(forcast);
-
+})
